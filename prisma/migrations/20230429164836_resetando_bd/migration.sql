bbda29291a3ea54b1551b2e6581f703a4720a19b
diogorodriguees <diogorodrigueslife@gmail.com>
2023-04-29 13:54:47 -0300

    :rocket: reset do bd

--- prisma/migrations/20230429164836_resetando_bd/migration.sql
+++ prisma/migrations/20230429164836_resetando_bd/migration.sql
@@ -1,17 +1,17 @@
 -- CreateTable
 CREATE TABLE "entrada" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "descricao" TEXT NOT NULL,
-    "valor" REAL NOT NULL,
     "tipo" TEXT NOT NULL,
+    "valor" REAL NOT NULL,
+    "descricao" TEXT NOT NULL,
     "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
 CREATE TABLE "saida" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "descricao" TEXT NOT NULL,
-    "valor" REAL NOT NULL,
     "tipo" TEXT NOT NULL,
+    "valor" REAL NOT NULL,
+    "descricao" TEXT NOT NULL,
     "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
