dafff4a47e32478b8246a05ede5f4f9665bd1cba
diogorodriguees <diogorodrigueslife@gmail.com>
2023-06-05 18:25:33 -0300

    :rocket: adicionando try catch

--- src/modules/controllers/tarefa.controller.ts
+++ src/modules/controllers/tarefa.controller.ts
@@ -1,66 +1,96 @@
 import { Request, Response } from "express";
-import { Tarefa } from "../functions/tarefa.functions";
+import { Tarefa } from "../repository/tarefa.repository";
 
 export class TarefaController {
     /* Cria uma nova tarefa e inseri ela no banco de dados */
     async criar(req: Request, res: Response) {
-        const { descricao } = req.body;
+        try {
+            const { descricao } = req.body;
 
-        const inserirTarefa = new Tarefa();
-        const tarefa = await inserirTarefa.criar(descricao);
+            const inserirTarefa = new Tarefa();
+            const tarefa = await inserirTarefa.criar(descricao);
 
-        return res.json(tarefa);
+            res.status(200).send(tarefa);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Excluir a tarefa com base no ID */
     async excluir(req: Request, res: Response) {
-        const { id } = req.query;
+        try {
+            const { id } = req.query;
 
-        const inserirTarefa = new Tarefa();
-        const excluida = await inserirTarefa.excluir(Number(id));
+            const inserirTarefa = new Tarefa();
+            const excluida = await inserirTarefa.excluir(Number(id));
 
-        return res.json(excluida);
+            res.status(200).send(excluida);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Marca uma tarefa como concluida */
     async marcarComoConcluida(req: Request<number>, res: Response) {
-        const { id } = req.query;
-        const inserirTarefa = new Tarefa();
-        const tarefa = await inserirTarefa.marcarComoConcluida(Number(id));
+        try {
+            const { id } = req.query;
+            const inserirTarefa = new Tarefa();
+            const tarefa = await inserirTarefa.marcarComoConcluida(Number(id));
 
-        return res.json(tarefa);
+            res.status(200).send(tarefa);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Marca a tarefa como não concluida */
     async desmarcarComoConcluida(req: Request<number>, res: Response) {
-        const { id } = req.query;
-        const inserirTarefa = new Tarefa();
-        const tarefa = await inserirTarefa.desmarcarComoConcluida(Number(id));
+        try {
+            const { id } = req.query;
+            const inserirTarefa = new Tarefa();
+            const tarefa = await inserirTarefa.desmarcarComoConcluida(
+                Number(id)
+            );
 
-        return res.json(tarefa);
+            res.status(200).send(tarefa);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Buscar tarefas não concluidas */
     async buscarNaoConcluidas(req: Request, res: Response) {
-        const tarefas = new Tarefa();
-        const naoConcluidas = await tarefas.buscarNaoConcluidas();
+        try {
+            const tarefas = new Tarefa();
+            const naoConcluidas = await tarefas.buscarNaoConcluidas();
 
-        return res.json(naoConcluidas);
+            res.status(200).send(naoConcluidas);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Buscar tarefas concluidas */
     async buscarConcluidas(req: Request, res: Response) {
-        const tarefas = new Tarefa();
-        const concluidas = await tarefas.buscarConcluidas();
+        try {
+            const tarefas = new Tarefa();
+            const concluidas = await tarefas.buscarConcluidas();
 
-        return res.json(concluidas);
+            res.status(200).send(concluidas);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 
     /* Busca todas as tarefas */
     async buscarTodas(req: Request, res: Response) {
-        const tarefas = new Tarefa();
-        const todas = await tarefas.buscarTodas();
+        try {
+            const tarefas = new Tarefa();
+            const todas = await tarefas.buscarTodas();
 
-        return res.json(todas);
+            res.status(200).send(todas);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 }
