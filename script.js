3764d06ee1d1de85c5fefc34ab1fe335f30989b0
diogorodriguees <diogorodrigueslife@gmail.com>
2023-06-13 19:11:18 -0300

    :rocket: função cancelar adicionado ao modal '

--- script.js
+++ script.js
@@ -13,46 +13,42 @@ let opcaoAtiva = "timer";
 let timerClock = 0;
 var totalMilliseconds = 0;
 var interval;
-var timerInicializado = false;
+var timerAtivo = false;
+var cronometroAtivo = false;
+var cancelar = document.getElementById("cancelar")
 
 function iniciarCronometro() {
-    resetTimer();
-
-    iniciar.textContent = "Iniciar";
-
-
-    if (opcaoAtiva === "cronometro") startCronometro();
-    if (opcaoAtiva === "timer") startTimerInvert();
-}
-
-function startTimerInvert() {
-    timerInicializado = true;
-    interval = setInterval(startTimer, 10); // Atualiza o cronômetro a cada 10 milissegundos
-}
-
-function startTimer() {
-    timerClock -= 10;
-
-    if (timerClock <= 0 && totalMilliseconds <=0) {
+    // reseta
+    if (totalMilliseconds <= 0 && timerClock <= 0) {
         resetTimer();
     }
 
-    // Calcula o número de horas, arredondando para baixo
+    iniciar.textContent = "Iniciar";
+
+    if (opcaoAtiva === "cronometro") startCronometro();
+    if (opcaoAtiva === "timer") startTimer();
+}
+function startCronometro() {
+    if (!cronometroAtivo) interval = setInterval(updateCronometro, 10); // Atualiza o cronômetro a cada 10 milissegundos
+    cronometroAtivo = true;
+}
+function startTimer() {
+    timerAtivo = true;
+    interval = setInterval(updateTimer, 10); // Atualiza o cronômetro a cada 10 milissegundos
+}
+
+function updateTimer() {
+    timerClock -= 10;
+
+    if (timerClock <= 0 && totalMilliseconds <= 0) {
+        resetTimer();
+    }
+
     var hr = Math.floor(timerClock / 3600000);
-
-    // Calcula o número de minutos restantes
     var minutosRestantes = timerClock % 3600000;
-
-    // Calcula o número de minutos, arredondando para baixo
     var minutes = Math.floor(minutosRestantes / 60000);
-
-    // Calcula o número de segundos restantes
     var segundosRestantes = minutosRestantes % 60000;
-
-    // Calcula o número de segundos, arredondando para baixo
     var seconds = Math.floor(segundosRestantes / 1000);
-
-    // Calcula o número de milissegundos restantes
     var milliseconds = (segundosRestantes % 1000) / 10;
 
     milisegundos.textContent = pad(milliseconds, 2);
@@ -61,44 +57,52 @@ function startTimer() {
     horas.textContent = pad(hr, 2);
 }
 
-function startCronometro() {
-    interval = setInterval(updateTimer, 10); // Atualiza o cronômetro a cada 10 milissegundos
-}
-
 function stopTimer() {
-    if (milisegundos > 0 || timerClock > 0) {
+    if (totalMilliseconds > 0 || timerClock > 0) {
         iniciar.textContent = "Continuar";
-
         clearInterval(interval);
     }
 
-    timerInicializado = false;
+    timerAtivo = false;
 }
 
 function resetTimer() {
-    totalMilliseconds = 0;
+    // zerando os milisegundos do TIMER e do CRONOMETRO
     timerClock = 0;
-    iniciar.textContent = "Iniciar";
-    timerInicializado = false;
+    totalMilliseconds = 0;
 
+    // alterando o botao "continuar" para o estado inicial "iniciar"
+    iniciar.textContent = "Iniciar";
+    timerAtivo = false;
+    cronometroAtivo = false;
+
+    // finalizar o interbal e atualiza o display
     clearInterval(interval);
-    updateTimer(1);
+
+    // parametro 1 passado para evitar o incremento de 10 quando resetar
+    updateCronometro(1);
 }
 
-function updateTimer(number) {
+function updateCronometro(number) {
+    // função resetTimer() envia 1 como parametro para evitar atribuição ao resetar
     if (number !== 1) {
         totalMilliseconds += 10;
     }
 
+    // conventendo os milisegundos em HORA, MINUTO, SEGUNDO, MILISEGUNDO
     var milliseconds = Math.floor((totalMilliseconds % 1000) / 10);
     var seconds = Math.floor((totalMilliseconds / 1000) % 60);
     var minutes = Math.floor((totalMilliseconds / (1000 * 60)) % 60);
+    var hr = Math.floor(totalMilliseconds / 360000);
 
+    // adicionando os valores no display
     milisegundos.textContent = pad(milliseconds, 2);
     segundos.textContent = pad(seconds, 2);
     minutos.textContent = pad(minutes, 2);
+    horas.textContent = pad(hr, 2);
 }
 
+// Mantém o par de digitos para numeros com apenas 1 digito (Ex: 1 => 01)
 function pad(value, width) {
     value = value.toString();
     while (value.length < width) {
@@ -107,48 +111,75 @@ function pad(value, width) {
     return value;
 }
 
+// Prepara a ferramento para ser utilizada como CRONOMETRO
 function changeToCronometro() {
+    // altera o estado da ferramenta
     opcaoAtiva = "cronometro";
+
+    // colorização para visualização do estado ativo
     timerBtn.style.backgroundColor = "#1e57d4";
     cronometro.style.backgroundColor = "rgb(255, 255, 39)";
+
+    // zera o display para executar o estado atual
     resetTimer();
 }
 
+// Prepara a ferramento para ser utilizada como TIMER
 function changeToTimer() {
+    // torna o modal visivel
     document.getElementById("modal").style.display = "flex";
+
+    // altera o estado da ferramenta
     opcaoAtiva = "timer";
+
+    // colorização para visualização do estado ativo
     timerBtn.style.backgroundColor = "rgb(255, 255, 39)";
     cronometro.style.backgroundColor = "#1e57d4";
+
+    // zera o display para executar o estado ativo
     resetTimer();
 }
 
+// Receve os valores do display e tranforma em milisegundos
 function receiveTime() {
+    // Esconde o modal
     document.getElementById("modal").style.display = "none";
+
+    // recebendo os valores de cada input
     var hora = document.getElementById("horaInput").value;
     var min = document.getElementById("minInput").value;
     var seg = document.getElementById("segInput").value;
 
+    // convertendo os valores para milisegundos
     timerClock = hora * 360000 + min * 60000 + seg * 1000;
 
+    // reseta os valores para a proxima abertura do modal
     document.getElementById("horaInput").value = 0;
     document.getElementById("minInput").value = 0;
     document.getElementById("segInput").value = 0;
 
-    startTimerInvert();
+    // iniciar o TIMER
+    startTimer();
 }
 
+// Ativa o modal para inserir o tempo do TIMER
 function displayClick() {
     if (opcaoAtiva === "timer") {
         document.getElementById("modal").style.display = "flex";
     }
 }
 
-// Exemplo de uso
-
+// Ao clicar no display o modal para informar o tempo do timer ira aparecer
 display.addEventListener("click", displayClick);
+
 iniciarTimer.addEventListener("click", receiveTime);
+
+// Botoes dos estados (Timer e Cronometro)
 timerBtn.addEventListener("click", changeToTimer);
 cronometro.addEventListener("click", changeToCronometro);
-parar.addEventListener("click", stopTimer);
+
+// Funções disponiveis para cada estado(Cronometro e Timer)
 iniciar.addEventListener("click", iniciarCronometro);
+parar.addEventListener("click", stopTimer);
 resetar.addEventListener("click", resetTimer);
+cancelar.addEventListener("click", () => document.getElementById("modal").style.display = "none")
