ab29bf9f5aeb0ae61cbffbc48cf0d0652f6393d3
diogorodriguees <diogorodrigueslife@gmail.com>
2023-11-12 14:02:17 -0300

    :tada: starting project

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import { Col, Row } from "antd";
+
+export const Home: React.FC = () => {
+    return (
+        <>
+            <Row gutter={9}>
+                <Col>Logo</Col>
+                <Col>Search</Col>
+            </Row>
+        </>
+    );
+};
